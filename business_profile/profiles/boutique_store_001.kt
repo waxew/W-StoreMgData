ee45b21638ab830f2a09876ca0ab4ed5dc48ae20
waxew <4622.ar@gmail.com>
2026-09-07 16:16:40 +0330

    Add boutique store business profile

---
+++ business_profile/profiles/boutique_store_001.kt
@@ -0,0 +1,14 @@
+package business_profile.profiles
+
+/**
+ * نام فایل: boutique_store_001.kt
+ * ماژول: Business Profile
+ * نوع: فروشگاه پوشاک
+ */
+
+val boutiqueStoreProfile = mapOf(
+    "id" to "boutique_store_001",
+    "type" to "BOUTIQUE_STORE",
+    "modules" to "customer,product,inventory,sales,invoice",
+    "attributes" to "brand,size,color,season,material"
+)
