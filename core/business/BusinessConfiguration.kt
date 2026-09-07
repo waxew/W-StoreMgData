92d98d96a16d8670a2e3f69b67ca6c7b1a5a434e
waxew <4622.ar@gmail.com>
2026-09-07 19:26:09 +0330

    Add Business configuration model

---
+++ core/business/BusinessConfiguration.kt
@@ -0,0 +1,20 @@
+package core.business
+
+/*
+================================================
+نام فایل:
+BusinessConfiguration.kt
+
+وظیفه:
+نگهداری تنظیمات عمومی یک نمونه نرم‌افزار.
+
+این بخش برای تنظیمات قابل تغییر بدون تغییر Core استفاده می‌شود.
+
+================================================
+*/
+
+data class BusinessConfiguration(
+    val themeId: String,
+    val language: String,
+    val currency: String
+)
