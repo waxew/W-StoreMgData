7a1249724926b6a56d6fe7e545163d57d71be88a
waxew <4622.ar@gmail.com>
2026-09-07 19:37:06 +0330

    add root gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+/*
+================================================
+نام فایل:
+build.gradle.kts
+
+وظیفه:
+تنظیمات اصلی Gradle پروژه.
+
+نسخه های نهایی و پلاگین های اندروید در این لایه مدیریت می شوند.
+================================================
+*/
+
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+}
