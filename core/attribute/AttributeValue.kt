9e12b1915e32968b092d4514cb7906be738d1fb2
waxew <4622.ar@gmail.com>
2026-09-07 18:05:03 +0330

    Add dynamic attribute value model

---
+++ core/attribute/AttributeValue.kt
@@ -0,0 +1,35 @@
+/*
+================================================
+نام فایل:
+AttributeValue.kt
+
+بخش:
+Core Attribute Engine
+
+وظیفه:
+نگهداری مقدار واقعی Attribute برای هر رکورد.
+
+مثال:
+Attribute:
+Color
+
+Value:
+Black
+
+Attribute:
+Storage
+
+Value:
+256GB
+================================================
+*/
+
+package com.wstoremgdata.core.attribute
+
+/**
+ * مقدار یک ویژگی پویا
+ */
+data class AttributeValue(
+    val attributeId: String,
+    val value: String
+)
