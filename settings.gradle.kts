a4239fe650c73f34e0155678ed5c52eb9e847968
waxew <4622.ar@gmail.com>
2026-09-07 19:37:01 +0330

    add android gradle settings foundation

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+/*
+================================================
+نام فایل:
+settings.gradle.kts
+
+وظیفه:
+تعریف ساختار ماژول های پروژه اندروید.
+
+این فایل نقطه شروع Gradle برای Android Studio است.
+================================================
+*/
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "W-StoreMgData"
+include(":app")
