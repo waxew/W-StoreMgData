0406b9cbd10ca12060095409922f8fddc2fd779f
waxew <4622.ar@gmail.com>
2026-09-07 19:32:41 +0330

    Add product dynamic attribute relation model

---
+++ core/product/ProductAttributeRelation.kt
@@ -0,0 +1,32 @@
+/*
+================================================
+نام فایل:
+ProductAttributeRelation.kt
+
+وظیفه:
+اتصال کالا به Attribute های پویا.
+
+مثال:
+Mobile Profile:
+Storage = 256GB
+Color = Black
+
+Boutique Profile:
+Size = XL
+Color = White
+
+این اطلاعات داخل ProductEntity قرار نمی‌گیرند.
+
+================================================
+*/
+
+package core.product
+
+/**
+ * ارتباط بین محصول و ویژگی های پویا.
+ */
+data class ProductAttributeRelation(
+    val productId: String,
+    val attributeId: String,
+    val value: String
+)
