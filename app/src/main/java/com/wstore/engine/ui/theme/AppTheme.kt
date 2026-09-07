28df5863e814f0d1b6392fcfd8232f1515be7ed9
waxew <4622.ar@gmail.com>
2026-09-07 19:54:59 +0330

    Add Compose theme foundation with Persian documentation comments

---
+++ app/src/main/java/com/wstore/engine/ui/theme/AppTheme.kt
@@ -0,0 +1,8 @@
+package com.wstore.engine.ui.theme
+
+// این فایل مسئول ساخت Theme اصلی رابط کاربری است.
+// در نسخه نهایی رنگ ها و ظاهر می توانند از Business Profile خوانده شوند.
+
+object AppThemeConfig {
+    const val DEFAULT_THEME = "default"
+}
