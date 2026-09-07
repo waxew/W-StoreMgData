b87df2afe0807cd9a38101bd757337aab4d30152
waxew <4622.ar@gmail.com>
2026-09-07 18:05:09 +0330

    Add module type definition

---
+++ core/module/ModuleType.kt
@@ -0,0 +1,33 @@
+/*
+================================================
+نام فایل:
+ModuleType.kt
+
+بخش:
+Core Module Engine
+
+وظیفه:
+تعریف دسته بندی ماژول های سیستم.
+
+ماژول ها قابلیت هایی هستند که دارای منطق و رفتار هستند.
+
+نمونه:
+Warranty
+Repair
+Delivery
+Appointment
+
+این فایل نباید شامل نوع کسب و کار باشد.
+================================================
+*/
+
+package com.wstoremgdata.core.module
+
+/**
+ * انواع کلی ماژول های سیستم
+ */
+enum class ModuleType {
+    CORE,
+    BUSINESS,
+    EXTENSION
+}
