d998bf4e9ecd9406444f9815a667c5044e10d71a
waxew <4622.ar@gmail.com>
2026-09-08 20:52:32 +0330

    Align customer repository contract with implementation

--- app/src/main/java/com/wstore/engine/domain/customer/CustomerRepository.kt
+++ app/src/main/java/com/wstore/engine/domain/customer/CustomerRepository.kt
@@ -1,5 +1,8 @@
 package com.wstore.engine.domain.customer
 
+import com.wstore.engine.data.customer.CustomerEntity
+
 interface CustomerRepository {
-    fun getCustomers(): List<Any>
+    suspend fun getCustomers(): List<CustomerEntity>
+    suspend fun saveCustomer(customer: CustomerEntity)
 }
