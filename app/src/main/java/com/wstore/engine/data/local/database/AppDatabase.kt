2be751e0c8019275d27465043d8dfa2b3d70a91e
waxew <4622.ar@gmail.com>
2026-09-09 17:16:07 +0330

    Add Room AppDatabase foundation

---
+++ app/src/main/java/com/wstore/engine/data/local/database/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.wstore.engine.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.wstore.engine.data.local.dao.ProductDao
+import com.wstore.engine.data.local.entity.ProductEntity
+
+@Database(
+    entities = [ProductEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+}
