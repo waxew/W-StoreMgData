8408b0a25ceb9f00bba9ac626c9bf62ffdad29a4
waxew <4622.ar@gmail.com>
2026-09-09 17:42:43 +0330

    Connect main activity to product feature screen

--- app/src/main/java/com/wstore/engine/MainActivity.kt
+++ app/src/main/java/com/wstore/engine/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.wstore.engine
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-class MainActivity: ComponentActivity(){override fun onCreate(b:Bundle?){super.onCreate(b);setContent{Text("W-StoreMgData")}}}
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.wstore.engine.ui.product.ProductScreen
+import com.wstore.engine.ui.product.ProductViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(b: Bundle?) {
+        super.onCreate(b)
+        setContent {
+            val viewModel: ProductViewModel = hiltViewModel()
+            ProductScreen(viewModel = viewModel)
+        }
+    }
+}
