23ef42aec4a76bbe2ad49322d9afc0decce7946f
waxew <4622.ar@gmail.com>
2026-09-09 17:20:39 +0330

    Add initial Compose Product screen for W-StoreMgData

---
+++ app/src/main/java/com/wstore/engine/ui/product/ProductScreen.kt
@@ -0,0 +1,13 @@
+package com.wstore.engine.ui.product
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductScreen() {
+    Column {
+        Text("Products")
+        Text("W-StoreMgData Product Module")
+    }
+}
