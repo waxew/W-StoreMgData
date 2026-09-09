6ad0c8726986b73bb9af4209d759ed27b1203f90
waxew <4622.ar@gmail.com>
2026-09-09 19:16:49 +0330

    Add invoice runtime module definition

---
+++ app/src/main/java/com/wstore/engine/ui/invoice/InvoiceModule.kt
@@ -0,0 +1,9 @@
+package com.wstore.engine.ui.invoice
+
+/**
+ * تعریف Feature Invoice برای Runtime Navigation.
+ */
+object InvoiceModule {
+    const val ID = "invoice"
+    const val ROUTE = "invoice"
+}
