2d448f2b68d96d22bfa18f43d1256ca6a751f998
waxew <4622.ar@gmail.com>
2026-09-09 17:57:51 +0330

    Register Kotlin JVM plugin for existing core engine module

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ build.gradle.kts
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
