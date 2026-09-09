d2daca7da125d707564199ddbfe450494824a991
waxew <4622.ar@gmail.com>
2026-09-10 02:25:17 +0330

    Add profile loader foundation

---
+++ business_profile/core/ProfileLoader.kt
@@ -0,0 +1,11 @@
+package com.wstore.core.profile
+
+/**
+ * Loader پایه پروفایل ها
+ * مرحله بعد اتصال به JSON انجام می شود.
+ */
+class ProfileLoader {
+    fun load(profile: ProfileDefinition): ProfileDefinition {
+        return profile
+    }
+}
