5ce9d3168edb3ec022a0d7c62af14efe4c36edf3
waxew <4622.ar@gmail.com>
2026-09-10 18:04:03 +0330

    Add Product Dynamic Form Screen foundation

---
+++ business_profile/ui/compose/ProductDynamicFormScreen.kt
@@ -0,0 +1,14 @@
+package business_profile.ui.compose
+
+/**
+ * صفحه فرم محصول پویا
+ *
+ * این فایل نقطه اتصال Product Create/Edit با Dynamic Form Renderer است.
+ * UI بر اساس Business Profile و Schema ساخته می‌شود.
+ */
+
+class ProductDynamicFormScreen {
+    fun render() {
+        // Compose implementation is connected in UI layer.
+    }
+}
