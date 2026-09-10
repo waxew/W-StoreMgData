fbd30e82ffdd58f7eda298deb48eb78ba55967f5
waxew <4622.ar@gmail.com>
2026-09-10 18:43:34 +0330

    Add mobile product dynamic form section component

---
+++ app/src/main/java/com/wstore/engine/ui/profile/mobile_store_001/components/MobileProductFormSection.kt
@@ -0,0 +1,14 @@
+package com.wstore.engine.ui.profile.mobile_store_001.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * بخش نمایش فیلدهای پویا در فرم محصول موبایل.
+ *
+ * داده‌ها باید از Profile Schema Loader دریافت شوند.
+ */
+@Composable
+fun MobileProductFormSection() {
+    Text(text = "Dynamic Product Attributes")
+}
