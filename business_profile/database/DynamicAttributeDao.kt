a9f99aab1ddbf540e26ef8c8eb74abcc4b06a9cf
waxew <4622.ar@gmail.com>
2026-09-10 16:13:07 +0330

    Extend DynamicAttribute DAO contract

--- business_profile/database/DynamicAttributeDao.kt
+++ business_profile/database/DynamicAttributeDao.kt
@@ -2,8 +2,14 @@ package business_profile.database
 
 /**
  * قرارداد دسترسی به ذخیره سازی Attribute های پویا.
+ * این لایه مستقل از Room نگه داشته می شود تا معماری قابل توسعه باشد.
  */
 interface DynamicAttributeDao {
-    fun save(entity: DynamicAttributeEntity)
-    fun findByOwner(ownerId: String): List<DynamicAttributeEntity>
+    suspend fun save(entity: DynamicAttributeEntity)
+
+    suspend fun update(entity: DynamicAttributeEntity)
+
+    suspend fun delete(attributeId: String)
+
+    suspend fun findByOwner(ownerId: String): List<DynamicAttributeEntity>
 }
