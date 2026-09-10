a2ad5230ffe3343756f40d97a8f37ecb84328138
waxew <4622.ar@gmail.com>
2026-09-10 18:43:24 +0330

    Add mobile_store_001 product form screen foundation

---
+++ app/src/main/java/com/wstore/engine/ui/profile/mobile_store_001/product/MobileProductFormScreen.kt
@@ -0,0 +1,19 @@
+package com.wstore.engine.ui.profile.mobile_store_001.product
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * فرم ایجاد و ویرایش محصول پروفایل فروشگاه موبایل.
+ *
+ * فیلدهای اختصاصی مانند IMEI، RAM، Storage و Warranty
+ * باید از Dynamic Schema دریافت شوند و در این لایه Hard Code نشوند.
+ */
+@Composable
+fun MobileProductFormScreen() {
+    // TODO:
+    // اتصال به DynamicFormRenderer
+    // اتصال به ProductViewModel
+    // اتصال Save Flow به Repository
+    Text(text = "Mobile Product Form")
+}
