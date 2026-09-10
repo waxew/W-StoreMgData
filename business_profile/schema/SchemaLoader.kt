92599da171ac3e14800230e73a89dcc2a58e5178
waxew <4622.ar@gmail.com>
2026-09-10 14:24:00 +0330

    Add dynamic schema loader foundation

---
+++ business_profile/schema/SchemaLoader.kt
@@ -0,0 +1,16 @@
+package business_profile.schema
+
+/**
+ * مسئولیت:
+ * مدیریت بارگذاری Schema مربوط به فیلدهای اختصاصی هر Business Profile.
+ *
+ * مثال:
+ * Mobile -> IMEI, Warranty
+ * Boutique -> Size, Color
+ */
+class SchemaLoader {
+
+    fun load(): List<AttributeDefinition> {
+        return emptyList()
+    }
+}
